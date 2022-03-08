e777bb79909e6ed4007e8360542386033d6be41d
Sebastian Segui Sanchez <sebas@correo.com>
2022-03-08 16:33:39 +0100

    acabado

--- hojasCSS/src/main/java/es/ideas/hojascss/App.java
+++ hojasCSS/src/main/java/es/ideas/hojascss/App.java
@@ -8,17 +8,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 600, 400);
+        stage.setTitle("Hojas de estilo");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
@@ -34,5 +33,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
--- hojasCSS/src/main/java/es/ideas/hojascss/PrimaryController.java
+++ hojasCSS/src/main/java/es/ideas/hojascss/PrimaryController.java
@@ -1,12 +1,46 @@
 package es.ideas.hojascss;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private AnchorPane pane;
+    @FXML
+    private Button btnHoja1;
+    @FXML
+    private Button btnHoja2;
+    @FXML
+    private Button btnHoja3;
+    @FXML
+    private Button btnHoja4;
+    @FXML
+    private Label lblTexto;
+
+    @FXML
+    private void hojaEstilos1(ActionEvent event) {
+        pane.getStylesheets().clear();
+        pane.getStylesheets().add("es/ideas/css/hoja1.css");
+    }
+    
+    @FXML
+    private void hojaEstilos2(ActionEvent event) {
+        pane.getStylesheets().clear();
+        pane.getStylesheets().add("es/ideas/css/hoja2.css");
+    }
+
+    @FXML
+    private void hojaEstilos3(ActionEvent event) {
+        pane.getStylesheets().clear();
+        pane.getStylesheets().add("es/ideas/css/hoja3.css");
+    }
+
+    @FXML
+    private void hojaEstilos4(ActionEvent event) {
+        pane.getStylesheets().clear(); //Hoja por defecto sin estilo alguno
     }
 }
